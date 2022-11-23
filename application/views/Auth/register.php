9da6ccf775043682711a4fb06fa4fb5bebacf805
Adyadma Renjiro <Adyadma Renjiro>
2022-11-23 20:10:09 +0700

    sip

--- application/views/Auth/register.php
+++ application/views/Auth/register.php
@@ -1,1 +1,44 @@
-<h1>register</h1>
+<section>
+    <div class="container mt-5 py-5">
+        <div class=" mb-5">
+            <div class="col-12 col-sm-8 col-md-6 m-auto">
+                <div class="card shadow rounded px-4">
+                    <div class="card-body">
+                        <h3 class="card-title py-4">Sign Up</h3>
+                        <form action="">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="username">
+                                <label for="floatingInput">Username</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floatingInput" placeholder="na">
+                                <label for="floatingInput">Email</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="floatingInput" placeholder="111">
+                                <label for="floatingInput">Phone Number</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Repeat Password</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    I agree to the <b><a href="" style="text-decoration: none; color: rgb(30, 30, 30);">Terms of User</a> </b>
+                                </label>
+                            </div>
+                            <div class="text-center d-grid gap-2 mt-4">
+                                <button class="btn btn-primary py-2 ">Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
