9cc644184c0066c173c87bad48b8edebe7c60fda
Adyadma Renjiro <Adyadma Renjiro>
2022-11-23 15:51:44 +0700

    log

--- application/views/Auth/login.php
+++ application/views/Auth/login.php
@@ -1,1 +1,15 @@
-<h1>login</h1>
+<section>
+    <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-6 m-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="">
+                            <input type="text" class="text form-control my-3 py-2" placeholder="username">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
